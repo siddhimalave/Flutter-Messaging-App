e5e9db965ce7c4c963fbd5b9c724d00674056600
Siddhi Malave <87321644+siddhimalave@users.noreply.github.com>
2022-03-02 21:33:28 +0530

    Create TextonClickButton.dart
    On clicking button we can view the typed text

---
+++ TextonClickButton.dart
@@ -0,0 +1,82 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messaging Application',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "";
+
+  void changeText(String text) {
+    this.setState(() {
+      this.text = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Messaging Application')),
+        body: Column(
+            children: <Widget>[TextInput(this.changeText), Text(this.text)]));
+  }
+}
+
+class TextInput extends StatefulWidget {
+  final Function(String) callback;
+  TextInput(this.callback);
+
+  @override
+  _TextInputState createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.message),
+            labelText: "Type a message : ",
+            suffixIcon: IconButton(
+              icon: Icon(Icons.send),
+              splashColor: Colors.deepPurpleAccent,
+              tooltip: "Post message",
+              onPressed: this.click,
+            )));
+  }
+}
